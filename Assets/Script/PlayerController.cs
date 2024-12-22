f3e60013e57ed757c60c43b91902fd25618c9b92
Faiz2979 <faizannabil877@gmail.com>
2024-12-22 07:29:14 +0700

    Update PlayerController.cs

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -65,15 +65,20 @@ public class PlayerController : MonoBehaviour
 
         if (player_Attack && attackTimer < 0){
             
-            if(comboTimer <= 0){
-            comboCounter = 1;
-            }
+        if (comboTimer > 0) {
+            // Trigger animasi serangan berikutnya dalam combo
             animator.SetTrigger("isAttacking");
+        } else {
+            // Combo telah selesai, reset ke serangan awal
+            comboCounter = 1; 
+            animator.SetTrigger("isAttacking");
+        }
 
             comboTimer = comboCD;
             attackTimer = attackCD;
 
         }
+
         player_Jump = playerControls.Movement.Jump.triggered;
         player_Down = playerControls.Movement.Down.triggered;
     }
